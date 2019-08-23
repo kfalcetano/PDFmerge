6fc853062f735c1fbdc4450eada8f59acacfadd6
kfalcetano <kevin.falcetano@gmail.com>
2019-08-23 11:04:15 -0500

    commit python 2.7 version

--- PDFmerge_tk.py
+++ PDFmerge_tk.py
@@ -1,46 +1,42 @@
-import tkinter as tk
-from tkinter.filedialog import askopenfilename
-from tkinter.filedialog import asksaveasfilename
-from tkinter.scrolledtext import ScrolledText
-from tkinter.font import Font
-from tkinter import messagebox
+from Tkinter import *
+import Tkinter, Tkconstants, tkFileDialog, tkMessageBox, tkFont, ScrolledText
 from PyPDF2 import PdfFileMerger
 import os
 
-class Application(tk.Frame):
+class Application(Tkinter.Frame):
     def __init__(self, master=None):
-        super().__init__(master)
+        Frame.__init__(self,master)
         self.master = master
         self.pack(fill="both", expand=1)
         self.filenames = list()
         self.create_widgets()
 
     def create_widgets(self):
-        bfont = Font(family="Helvetica",size = 10)
-        self.flist = ScrolledText(self, height=12, width = 80)
+        bfont = tkFont.Font(family="Helvetica",size = 10)
+        self.flist = ScrolledText.ScrolledText(self, height=12, width = 80)
         self.flist["font"] = bfont
         self.flist.pack(side="left",fill="both",expand=1)
-        self.flist.insert(tk.END,"PDF Paths Listed Here:\n\n")
+        self.flist.insert(Tkinter.END,"PDF Paths Listed Here:\n\n")
         self.flist.config(state="disabled")
         
-        self.right = tk.Frame(self)
+        self.right = Tkinter.Frame(self)
         self.right.pack(side="right",padx=20)
 
-        self.browse = tk.Button(master=self.right)
+        self.browse = Tkinter.Button(master=self.right)
         self.browse["text"] = "Add a PDF"
         self.browse["command"] = self.get_file
         self.browse["font"] = bfont
         self.browse.configure(bg = "light gray")
         self.browse.pack(pady=20,ipadx=5,ipady=5)
 
-        self.merge = tk.Button(master=self.right)
+        self.merge = Tkinter.Button(master=self.right)
         self.merge["text"]= "Merge"
         self.merge["command"] = self.merge_files
         self.merge["font"] = bfont
         self.merge.configure(bg = "light gray")
         self.merge.pack(side="bottom",pady=35,ipadx=18,ipady=10)
 
-        self.clear = tk.Button(master=self.right)
+        self.clear = Tkinter.Button(master=self.right)
         self.clear["text"] = "Clear List"
         self.clear["command"] = self.clear_list
         self.clear["font"] = bfont
@@ -51,10 +47,10 @@ class Application(tk.Frame):
         print("hi there, everyone!")
 
     def get_file(self):
-        filename = askopenfilename(title = "Select PDFs",filetypes=(("PDF files","*.pdf"),("All Files","*.*")))
+        filename = tkFileDialog.askopenfilename(title = "Select PDFs",filetypes=(("PDF files","*.pdf"),("All Files","*.*")))
         if filename != "":
             self.flist.config(state="normal")
-            self.flist.insert(tk.END,str(self.filenames.__len__()+1)+". "+filename+"\n")
+            self.flist.insert(Tkinter.END,str(self.filenames.__len__()+1)+". "+filename+"\n")
             self.filenames.append(filename)
             self.flist.config(state="disabled")
 
@@ -62,7 +58,7 @@ class Application(tk.Frame):
         if len(self.filenames)==0:
             messagebox.showerror(title="Error",message="Plese select at least one file.")
         else:
-            filename = asksaveasfilename(filetypes=(("PDF files","*.pdf"),("All Files","*.*")),defaultextension=".pdf")
+            filename = tkFileDialog.asksaveasfilename(filetypes=(("PDF files","*.pdf"),("All Files","*.*")),defaultextension=".pdf")
             if filename != "":
                 merger(filename,self.filenames) 
                 self.clear_list()
@@ -73,8 +69,8 @@ class Application(tk.Frame):
     def clear_list(self):
         self.flist.config(state="normal")
         del self.filenames[:]
-        self.flist.delete(1.0,tk.END)
-        self.flist.insert(tk.END,"PDF Paths Listed Here:\n\n")
+        self.flist.delete(1.0,Tkinter.END)
+        self.flist.insert(Tkinter.END,"PDF Paths Listed Here:\n\n")
         self.flist.config(state="disabled")
 
 def merger(output_path, input_paths):
@@ -86,7 +82,7 @@ def merger(output_path, input_paths):
     with open(output_path, 'wb') as fileobj:
         pdf_merger.write(fileobj)
 
-root = tk.Tk(className=" PDF Merge")
+root = Tkinter.Tk(className=" PDF Merge")
 root.iconbitmap("icons/ayylmao2.ico")
 app = Application(master=root)
 app.mainloop()
